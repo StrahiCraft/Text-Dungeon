1cf0b030bd05f9f4862a0e0745df0bdb31a67ea5
StrahiCraft <strahi04@gmail.com>
2025-06-28 15:21:49 +0200

    refactored DungeonMapRenderer.java

--- src/dungeon/DungeonMapRenderer.java
+++ src/dungeon/DungeonMapRenderer.java
@@ -8,32 +8,9 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class DungeonMapRenderer {
+
     public static void renderDungeonMap(){
-        DungeonBounds dungeonBounds = Dungeon.getDungeonBounds();
-
-        char[][] dungeonMap = new char[(dungeonBounds.getMaxDungeonCoordinate().getY() -
-                dungeonBounds.getMinDungeonCoordinate().getY() + 1) * 3]
-                [(dungeonBounds.getMaxDungeonCoordinate().getX() -
-                dungeonBounds.getMinDungeonCoordinate().getX() + 1) * 3];
-
-        for(int i = 0; i < dungeonMap.length; i++){
-            Arrays.fill(dungeonMap[i], ' ');
-        }
-
-        for(DungeonRoom dungeonRoom : Collections.list(Dungeon.getDungeonRooms().elements())){
-            dungeonMap[(dungeonRoom.getPosition().getY() - dungeonBounds.getMinDungeonCoordinate().getY()) * 3 + 1]
-                    [(dungeonRoom.getPosition().getX() - dungeonBounds.getMinDungeonCoordinate().getX()) * 3 + 1] = '#';
-
-            if(dungeonRoom.getPosition().getX() == 0 && dungeonRoom.getPosition().getY() == 0){
-                dungeonMap[(dungeonRoom.getPosition().getY() - dungeonBounds.getMinDungeonCoordinate().getY()) * 3 + 1]
-                        [(dungeonRoom.getPosition().getX() - dungeonBounds.getMinDungeonCoordinate().getX()) * 3 + 1] = 'S';
-            }
-
-            renderHallways(dungeonRoom, Vector2Int.up(), dungeonMap, dungeonBounds);
-            renderHallways(dungeonRoom, Vector2Int.down(), dungeonMap, dungeonBounds);
-            renderHallways(dungeonRoom, Vector2Int.left(), dungeonMap, dungeonBounds);
-            renderHallways(dungeonRoom, Vector2Int.right(), dungeonMap, dungeonBounds);
-        }
+        char[][] dungeonMap = fillDungeonMap();
 
         for(int x = dungeonMap.length - 1; x > 0; x--){
             for(int y = 0; y < dungeonMap[x].length; y++){
@@ -44,7 +21,41 @@ public class DungeonMapRenderer {
         System.out.println();
     }
 
-    private static void renderHallways(DungeonRoom currentRoom, Vector2Int direction, char[][] dungeonMap, DungeonBounds dungeonBounds) {
+    private static char[][] fillDungeonMap(){
+        DungeonBounds dungeonBounds = Dungeon.getDungeonBounds();
+
+        char[][]dungeonMap = new char[(dungeonBounds.getMaxDungeonCoordinate().getY() -
+                dungeonBounds.getMinDungeonCoordinate().getY() + 1) * 3]
+                [(dungeonBounds.getMaxDungeonCoordinate().getX() -
+                dungeonBounds.getMinDungeonCoordinate().getX() + 1) * 3];
+
+        for(int i = 0; i < dungeonMap.length; i++){
+            Arrays.fill(dungeonMap[i], ' ');
+        }
+
+        for(DungeonRoom currentRoom : Collections.list(Dungeon.getDungeonRooms().elements())){
+            fillDungeonRoom(currentRoom, dungeonMap, dungeonBounds);
+        }
+
+        return dungeonMap;
+    }
+
+    private static void fillDungeonRoom(DungeonRoom currentRoom, char[][] dungeonMap, DungeonBounds dungeonBounds){
+        dungeonMap[(currentRoom.getPosition().getY() - dungeonBounds.getMinDungeonCoordinate().getY()) * 3 + 1]
+                [(currentRoom.getPosition().getX() - dungeonBounds.getMinDungeonCoordinate().getX()) * 3 + 1] = '#';
+
+        if(currentRoom.getPosition().getX() == 0 && currentRoom.getPosition().getY() == 0){
+            dungeonMap[(currentRoom.getPosition().getY() - dungeonBounds.getMinDungeonCoordinate().getY()) * 3 + 1]
+                    [(currentRoom.getPosition().getX() - dungeonBounds.getMinDungeonCoordinate().getX()) * 3 + 1] = 'S';
+        }
+
+        fillHallways(currentRoom, Vector2Int.up(), dungeonMap, dungeonBounds);
+        fillHallways(currentRoom, Vector2Int.down(), dungeonMap, dungeonBounds);
+        fillHallways(currentRoom, Vector2Int.left(), dungeonMap, dungeonBounds);
+        fillHallways(currentRoom, Vector2Int.right(), dungeonMap, dungeonBounds);
+    }
+
+    private static void fillHallways(DungeonRoom currentRoom, Vector2Int direction, char[][] dungeonMap, DungeonBounds dungeonBounds) {
         if(currentRoom.getRoom(direction) == null){
             return;
         }
