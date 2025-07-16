94c9eb924b8fbbbcbb5bc7f0f6665fd3efe484cb
StrahiCraft <strahi04@gmail.com>
2025-07-16 15:02:43 +0300

    removed combat actions

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,6 +1,5 @@
 package entity;
 
-import entity.actions.CombatAction;
 import utility.Stats;
 
 import java.util.ArrayList;
@@ -9,24 +8,14 @@ public abstract class Entity {
     private String name;
     private Stats stats;
 
-    private ArrayList<CombatAction> combatActions;
-
     public Entity() {
         name = "Entity";
         stats = new Stats();
-        combatActions = new ArrayList<CombatAction>();
     }
 
     public Entity(String name, Stats stats) {
         this.name = name;
         this.stats = stats;
-        combatActions = new ArrayList<CombatAction>();
-    }
-
-    public Entity(String name, Stats stats, ArrayList<CombatAction> combatActions) {
-        this.name = name;
-        this.stats = stats;
-        this.combatActions = combatActions;
     }
 
     public void takeDamage(float damage) {
@@ -46,23 +35,6 @@ public abstract class Entity {
 
     public void useSpeed(float amount){
         getStats().setCurrentSpeed(getStats().getCurrentSpeed() - amount);
-    }
-
-    public void addCombatAction(CombatAction newAction){
-        combatActions.add(newAction);
-    }
-
-    public void removeCombatAction(CombatAction action){
-        removeCombatAction(action.getName());
-    }
-
-    public void removeCombatAction(String actionName){
-        for(int i = 0; i < combatActions.size(); i++){
-            if(combatActions.get(i).getName().equals(actionName)){
-                combatActions.remove(i);
-                return;
-            }
-        }
     }
 
     public void increaseStats(Stats increase) {
@@ -98,14 +70,6 @@ public abstract class Entity {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public ArrayList<CombatAction> getCombatActions() {
-        return combatActions;
-    }
-
-    public void setCombatActions(ArrayList<CombatAction> combatActions) {
-        this.combatActions = combatActions;
     }
 
     @Override
